26d6e9b1bcfe803165edbe3424c1b48bbb2386a8
Joofer <joofer.sp@gmail.com>
2023-09-01 18:24:31 +0300

    Added repository abstractions

---
+++ Domain/RepositoryAbstractions/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.RepositoryAbstractions;
+
+public interface IRepositoryBase<TEntity>
+{
+    Task<IEnumerable<TEntity>> GetAsync(CancellationToken cancellationToken = default);
+    Task<TEntity> FindAsync(object keyValue, CancellationToken cancellationToken = default);
+    Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken = default);
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task RemoveAsync(object keyValue, CancellationToken cancellationToken = default);
+}
