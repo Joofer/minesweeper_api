d55a52982fb6e9ce99c844e8cfde828d9531c046
Joofer <joofer.sp@gmail.com>
2023-09-02 01:22:27 +0300

    Added OriginField

--- Domain/Entities/GameInfo.cs
+++ Domain/Entities/GameInfo.cs
@@ -8,5 +8,6 @@ public class GameInfo
     public int MinesCount { get; set; }
     public bool Completed { get; set; }
     public List<List<string>> Field { get; set; } = new();
+    public List<List<string>> OriginField { get; set; } = new();
     public List<TurnInfo> TurnInfos { get; set; } = new();
 }
