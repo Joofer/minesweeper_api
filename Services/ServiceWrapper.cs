57d668e481084c891a4589a5f49a6f4cd36f5f28
Joofer <joofer.sp@gmail.com>
2023-09-01 19:04:53 +0300

    Added services

---
+++ Services/ServiceWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Domain.RepositoryAbstractions;
+using Services.Abstractions;
+
+namespace Services;
+
+public sealed class ServiceWrapper : IServiceWrapper
+{
+    private readonly Lazy<IGameInfoService> _gameInfoService;
+    private readonly Lazy<ITurnInfoService> _turnInfoService;
+
+    public ServiceWrapper(IRepositoryWrapper repository)
+    {
+        _gameInfoService = new Lazy<IGameInfoService>(() => new GameInfoService(repository));
+        _turnInfoService = new Lazy<ITurnInfoService>(() => new TurnInfoService(repository));
+    }
+
+    public IGameInfoService GameInfoService => _gameInfoService.Value;
+    public ITurnInfoService TurnInfoService => _turnInfoService.Value;
+}
---
+++ Services/TurnInfoService.cs
@@ -0,0 +1,20 @@
+﻿using Contracts.Requests;
+using Domain.RepositoryAbstractions;
+using Services.Abstractions;
+
+namespace Services;
+
+internal sealed class TurnInfoService : ITurnInfoService
+{
+    private readonly IRepositoryWrapper _repository;
+
+    public TurnInfoService(IRepositoryWrapper repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task AddTurnAsync(GameTurnRequest gameTurnRequest, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
