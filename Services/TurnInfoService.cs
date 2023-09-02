e991d1e85c69ce5bf21cf50c9ef4943edb41ec29
Joofer <joofer.sp@gmail.com>
2023-09-02 14:33:22 +0300

    TurnInfoService returns GameInfoResponse

--- Services/TurnInfoService.cs
+++ Services/TurnInfoService.cs
@@ -1,4 +1,7 @@
-﻿using Contracts.Requests;
+﻿using CollectionManager;
+using Contracts.Requests;
+using Contracts.Responses;
+using Domain.Entities;
 using Domain.Exceptions;
 using Domain.RepositoryAbstractions;
 using Minesweeper;
@@ -15,7 +18,8 @@ internal sealed class TurnInfoService : ITurnInfoService
         _repository = repository;
     }
 
-    public async Task AddTurnAsync(GameTurnRequest gameTurnRequest, CancellationToken cancellationToken = default)
+    public async Task<GameInfoResponse> AddTurnAsync(GameTurnRequest gameTurnRequest,
+        CancellationToken cancellationToken = default)
     {
         var gameInfo = await _repository.GameInfoRepository.FindAsync(gameTurnRequest.game_id, cancellationToken) ??
                        throw new GameInfoNotFoundException(gameTurnRequest.game_id);
@@ -23,9 +27,46 @@ internal sealed class TurnInfoService : ITurnInfoService
         if (gameInfo.Completed)
             throw new GameEndedException();
 
-        var result = TurnManager.Open(gameInfo.Field, gameInfo.OriginField, gameTurnRequest.col, gameTurnRequest.row);
-        if (result) gameInfo.Completed = false;
+        var turnInfo = new TurnInfo
+        {
+            Guid = Guid.NewGuid(),
+            Column = gameTurnRequest.col,
+            Row = gameTurnRequest.row,
+            DateTime = DateTime.Now
+        };
+
+        gameInfo.TurnInfos.Add(turnInfo);
+
+        var result = TurnManager.Open(gameInfo.Field, gameInfo.OriginField, gameInfo.MinesCount, gameTurnRequest.col, gameTurnRequest.row);
+        if (result != (int)TurnResponseCode.Ok) gameInfo.Completed = true;
 
         await _repository.SaveChangesAsync(cancellationToken);
+
+        var strField = List2d<int>.ToStringType(gameInfo.Field);
+        foreach (var row in strField)
+        {
+            for (var i = 0; i < row.Count; i++)
+            {
+                if (row[i] == BoxType.Closed.ToString())
+                    row[i] = row[i].Replace(BoxType.Closed.ToString(), " ");
+                else
+                    row[i] = result switch
+                    {
+                        (int)TurnResponseCode.Mine => row[i].Replace(BoxType.Mine.ToString(), "X"),
+                        (int)TurnResponseCode.End => row[i].Replace(BoxType.Mine.ToString(), "M"),
+                        _ => row[i]
+                    };
+            }
+        }
+
+        return new GameInfoResponse
+        {
+            game_id = gameInfo.Guid,
+            width = gameInfo.Width,
+            height = gameInfo.Height,
+            mines_count = gameInfo.MinesCount,
+            completed = gameInfo.Completed,
+            field = strField
+        };
     }
 }
