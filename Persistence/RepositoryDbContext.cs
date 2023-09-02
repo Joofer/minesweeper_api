9a99c9c0f8c090a4b355fbc731f2963fb33509a0
Joofer <joofer.sp@gmail.com>
2023-09-02 17:27:14 +0300

    Implemented GameInfoRepository

---
+++ Persistence/RepositoryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class RepositoryDbContext : DbContext
+{
+    public DbSet<GameInfo> GameInfos { get; set; } = null!;
+
+    public RepositoryDbContext(DbContextOptions<RepositoryDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
+    }
+}
