ce110f84bbf4478acaa3325467b86c73b6c751df
Joofer <joofer.sp@gmail.com>
2023-09-03 23:59:01 +0300

    Constructed services

--- minesweeper_api/Program.cs
+++ minesweeper_api/Program.cs
@@ -1,10 +1,33 @@
+using Domain.RepositoryAbstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using Persistence.Repositories;
+using Services;
+using Services.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RepositoryDbContext>(options =>
+    options.UseCosmos(builder.Configuration.GetSection("AzureCDB")["Endpoint"], builder.Configuration.GetSection("AzureCDB")["Key"], builder.Configuration.GetSection("AzureCDB")["Db"]));
+
+builder.Services.AddScoped<IServiceWrapper, ServiceWrapper>();
+builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+builder.Services.AddMvc().AddApplicationPart(typeof(Presentation.Controllers.GameInfoController).Assembly).AddControllersAsServices();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(setup => setup.SwaggerDoc("v1", new OpenApiInfo { Title = "Minesweeper API", Version = "v1" }));
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Minesweeper API v1"));
 
-app.UseRouting();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
