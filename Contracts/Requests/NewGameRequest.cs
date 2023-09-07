def6457bc3714e943b941b510628fff24df91d4c
Joofer <joofer.sp@gmail.com>
2023-09-07 12:43:21 +0300

    Request fields type changed to required

--- Contracts/Requests/NewGameRequest.cs
+++ Contracts/Requests/NewGameRequest.cs
@@ -3,7 +3,7 @@ namespace Contracts.Requests;
 
 public class NewGameRequest
 {
-    public int width { get; set; }
-    public int height { get; set; }
-    public int mines_count { get; set; }
+    public required int width { get; set; }
+    public required int height { get; set; }
+    public required int mines_count { get; set; }
 }
